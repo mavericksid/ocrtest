1a8e4d24b4d6d7200dd18799a72b1eb8868f692f
sidharthkhattri <sidharth.khattri@gmail.com>
2015-03-13 16:22:14 +0530

    Added image binarization as a part of image pre-processing

--- src/main/scala/ocrtest/TesseractExample.scala
+++ src/main/scala/ocrtest/TesseractExample.scala
@@ -7,35 +7,69 @@ import org.im4java.core.IMOperation
 import org.im4java.core.GMOperation
 import org.im4java.core.ConvertCmd
 
-object PreProcessImage extends App {
+object OcrTest extends App {
 
-  // create command
-  val cmd = new ConvertCmd
+  // pre-processes the image using ImageMagick
+  (new ImagePrePocessor).preProcessImage
 
-  // create the operation
-  val op = new IMOperation
-  op.addImage("src/main/resources/sample.jpg")
-  op.units("PixelsPerInch").density(300)
-  op.colorspace("gray")
-  op.p_dither
-  op.colors(2)
-  op.normalize
-  op.addImage("src/main/resources/out.jpg")
+  // Reads pre-processed image using tesseract
+  val result = (new OcrEngine).readTextFromImage
 
-  // execute the operation
-  cmd.run(op)
+  println("Result of OCR is:\n\n" + result)
+
 }
 
-object TesseractExample extends App {
+/**
+ * Does image pre-processing for
+ * enhancing and binarizing the image
+ * using ImagMagick
+ */
+class ImagePrePocessor {
 
-  // File Names OCR1.jpg, OCR2.jpg, OCR3.jpg, OCR4.jpg, OCR5.jpg
-  val imageFile = new File("src/main/resources/ocr1.jpg")
-  val tesseractInsatnce = Tesseract.getInstance
+  def preProcessImage = {
+    // create command
+    val cmd = new ConvertCmd
 
-  try {
-    val result = tesseractInsatnce.doOCR(imageFile)
-    println("Extracted Text from Image is:\n\n" + result)
-  } catch {
-    case ex: Exception => ex.printStackTrace
+    val grayScaleOpr = new IMOperation
+    grayScaleOpr.p_clone
+    grayScaleOpr.blur(0, 20)
+
+    // create the operation
+    val op = new IMOperation
+    op.addImage("src/main/resources/sample.jpg")
+    op.units("PixelsPerInch")
+    op.density(600)
+    op.contrast
+    op.sharpen(1)
+    op.gaussianBlur(1)
+    op.unsharp(10, 4, 1, 0)
+    op.colorspace("gray")
+    op.addSubOperation(grayScaleOpr)
+    op.compose("Divide_Src")
+    op.composite
+    op.unsharp(10, 4, 1, 0)
+    op.enhance.enhance.enhance.enhance.enhance
+    op.addImage("src/main/resources/out.jpg")
+
+    // execute the operation
+    cmd.run(op)
+  }
+}
+
+/**
+ * Reads text from the pre-processed image
+ * using tess4j as a wrapper around tesseract
+ */
+class OcrEngine {
+
+  /**
+   * Converts the image to String
+   */
+  def readTextFromImage: Try[String] = {
+    Try {
+      val imageFile = new File("src/main/resources/out.jpg")
+      val tesseractInsatnce = Tesseract.getInstance
+      tesseractInsatnce.doOCR(imageFile)
+    }
   }
 }
